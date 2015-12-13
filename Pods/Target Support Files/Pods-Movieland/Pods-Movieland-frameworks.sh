9c1b3fc026d3cd66cfdab43936c7a99beb68c824
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2015-12-13 16:11:02 +0100

    Pods updated, added SwiftyJSON for easy parsing JSON structures

--- Pods/Target Support Files/Pods-Movieland/Pods-Movieland-frameworks.sh
+++ Pods/Target Support Files/Pods-Movieland/Pods-Movieland-frameworks.sh
@@ -87,9 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Movieland/Alamofire.framework"
   install_framework "Pods-Movieland/Keys.framework"
   install_framework "Pods-Movieland/Result.framework"
+  install_framework "Pods-Movieland/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Movieland/Alamofire.framework"
   install_framework "Pods-Movieland/Keys.framework"
   install_framework "Pods-Movieland/Result.framework"
+  install_framework "Pods-Movieland/SwiftyJSON.framework"
 fi
