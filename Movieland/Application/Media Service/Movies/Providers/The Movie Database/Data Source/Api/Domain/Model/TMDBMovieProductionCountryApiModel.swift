85bfef112a492e67bf4417b41dbccfa4976831e6
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2015-12-15 13:46:07 +0100

    Added movie results api models

---
+++ Movieland/Application/Media Service/Movies/Providers/The Movie Database/Data Source/Api/Domain/Model/TMDBMovieProductionCountryApiModel.swift
@@ -0,0 +1,15 @@
+//
+//  TMDBMovieProductionCountryApiModel.swift
+//  Movieland
+//
+//  Created by Oscar Duran on 14/12/15.
+//  Copyright © 2015 Skydev. All rights reserved.
+//
+
+import Foundation
+
+struct TMDBMovieProductionCountryApiModel: Model {
+    
+    let iso_3166_1: String
+    let name: String
+}
