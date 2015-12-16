152c7100d617f37c8cc87bb8187413128658f529
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2015-12-16 15:48:03 +0100

    Added test for TMDBMovieMediaServiceSpokenLanguageApiParser

---
+++ MovielandTests/Application/Media Service/Movies/Providers/The Movie Data Base/Domain/Parser/TMDBMovieMediaServiceSpokenLanguageApiParserSpec.swift
@@ -0,0 +1,33 @@
+//
+//  TMDBMovieMediaServiceSpokenLanguageApiParserSpec.swift
+//  Movieland
+//
+//  Created by Oscar Duran on 16/12/15.
+//  Copyright © 2015 Skydev. All rights reserved.
+//
+ 
+import Quick
+import Nimble
+
+@testable import Movieland
+
+class TMDBMovieMediaServiceSpokenLanguageApiParserSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("TMDBMovieMediaServiceSpokenLanguageApiParser") {
+            
+            let sut = TMDBMovieMediaServiceSpokenLanguageApiParser()
+            
+            it("should parse a single item") {
+                
+                let json: [String: AnyObject] = JSONLoader.fixture("single_spoken_language")
+                
+                let spokenLanguage: TMDBMovieSpokenLanguageApiModel = sut.parse(json)
+                
+                expect(spokenLanguage.iso_639_1).to(equal("en"))
+                expect(spokenLanguage.name).to(equal("English"))
+            }
+        }
+    }
+}
